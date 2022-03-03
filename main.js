6803105fc74ac502e7860031d1d17748391b678d
ADESH RAI <61568773+adeshrai6@users.noreply.github.com>
2022-03-02 19:32:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const countdown = document.querySelector(".countdown");
 
-const launchDate = new Date("Jan 1, 2022 13:00:00").getTime();
+const launchDate = new Date("Jan 1, 2025 13:00:00").getTime();
 
 //Update every second
 const intvl = setInterval(() => {
